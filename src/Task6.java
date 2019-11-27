8194632b29813dca142ef0ed27b50f28c6344ac4
Иван <pavlovi.v@ya.ru>
2019-11-27 14:37:23 +0300

    Добавлена задача №6

---
+++ src/Task6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+ * Задача № 6
+ * Написать программу, которая будет выполнять следующие действия:
+ * 1) Ввод трех чисел с клавиатуры x, y, z
+ * 2) Нахождение и вывод на экран среднего арифметического чисел x, y, z
+ * 3) Деление среднего арифметического на 2 без остатка
+ * 4) Если полученное число больше 3, то вывести на экран сообщение "Программа выполнена корректно"
+ */
+
+public class Task6 {
+    public static void main(String[] args) {
+        Scanner scaner = new Scanner (System.in);
+        int x = scaner.nextInt();
+        int y = scaner.nextInt();
+        int z = scaner.nextInt();
+        int a = (x + y + z ) / 3 ;
+        System.out.println(a);
+
+        if (a / 2 > 3) System.out.println("Программа выполнена корректно!");
+    }
+}
